1312c954c7819c1f2bd4fd87a781debce48274dc
GibranKhalil <gibrankhalil150904@gmail.com>
2026-01-30 09:01:30 -0300

    feat: multiplayer screen

--- src/modules/screens/main_menu/menu.js
+++ src/modules/screens/main_menu/menu.js
@@ -10,7 +10,11 @@ export default class MainMenuScreen extends ScreenBase {
     constructor() {
         super()
         this.buttons = null;
+        this.multiplayerButtons = null;
         this.selectedIndex = 0;
+        this.isMultiplayerMode = false;
+        this.isTransitioning = false;
+        this.targetMode = null;
 
         this.introAnimationDone = false;
         this.flashRequested = false;
@@ -169,6 +173,70 @@ export default class MainMenuScreen extends ScreenBase {
                 }
             }
         });
+
+        this.CHOOSE_PLAYERS_LABEL = Assets.image(`${ASSETS_PATH.UI}/choose_players.png`, {
+            optimize: true,
+            animConfig: {
+                x: (SCREEN_WIDTH - 293) / 2,
+                y: (SCREEN_HEIGHT - 90) / 2
+            }
+        })
+        this.BTN_2_PLAYERS = Assets.image(`${ASSETS_PATH.UI}/btn_2_players.png`, {
+            optimize: true,
+            animConfig: {
+                width: 141,
+                height: 114,
+                frameWidth: 141,
+                frameHeight: 114,
+                framesPerRow: 2,
+                x: 100,
+                y: SCREEN_HEIGHT - 114 * 2,
+                totalFrames: 2,
+                animations: { "normal": { start: 0, end: 0 }, "hover": { start: 1, end: 1 } }
+            }
+        })
+        this.BTN_3_PLAYERS = Assets.image(`${ASSETS_PATH.UI}/btn_3_players.png`, {
+            optimize: true,
+            animConfig: {
+                width: 135,
+                height: 114,
+                frameWidth: 135,
+                frameHeight: 114,
+                framesPerRow: 2,
+                x: this.BTN_2_PLAYERS.x + this.BTN_2_PLAYERS.width,
+                y: SCREEN_HEIGHT - 114 * 2,
+                totalFrames: 2,
+                animations: { "normal": { start: 0, end: 0 }, "hover": { start: 1, end: 1 } }
+            }
+        })
+        this.BTN_4_PLAYERS = Assets.image(`${ASSETS_PATH.UI}/btn_4_players.png`, {
+            optimize: true,
+            animConfig: {
+                height: 114,
+                width: 140,
+                frameWidth: 140,
+                frameHeight: 114,
+                framesPerRow: 2,
+                x: this.BTN_3_PLAYERS.x + this.BTN_3_PLAYERS.width,
+                y: SCREEN_HEIGHT - 114 * 2,
+                totalFrames: 2,
+                animations: { "normal": { start: 0, end: 0 }, "hover": { start: 1, end: 1 } }
+            }
+        })
+        this.BTN_BACK = Assets.image(`${ASSETS_PATH.UI}/btn_back.png`, {
+            optimize: true,
+            animConfig: {
+                height: 49,
+                width: 172,
+                frameWidth: 172,
+                frameHeight: 49,
+                framesPerRow: 2,
+                x: (SCREEN_WIDTH - 172) / 2,
+                y: SCREEN_HEIGHT - 49 * 2,
+                totalFrames: 2,
+                animations: { "normal": { start: 0, end: 0 }, "hover": { start: 1, end: 1 } }
+            }
+        })
     }
 
     onEnter(fromState) {
@@ -179,32 +247,73 @@ export default class MainMenuScreen extends ScreenBase {
             this.STREAM_MAIN_MENU.play();
         }
 
-        if(this.introAnimationDone){
-            this.introAnimationDone = false;
-        }
+        this.BG_BLUE_GUY.x = -this.BG_BLUE_GUY.width;
+        this.BG_WHITE_GUY.x = SCREEN_WIDTH;
+        this.introAnimationDone = false;
 
-        this.buttons = [this.BTN_PLAY, this.BTN_MULTIPLAYER, this.BTN_CREDITS, this.BTN_SCORES]
+        this.buttons = [this.BTN_PLAY, this.BTN_MULTIPLAYER, this.BTN_CREDITS, this.BTN_SCORES];
+        this.multiplayerButtons = [this.BTN_2_PLAYERS, this.BTN_3_PLAYERS, this.BTN_4_PLAYERS, this.BTN_BACK];
+        this.selectedIndex = 0;
+        this.isMultiplayerMode = false;
     }
 
     drawBackground() {
         this.BACKGROUND.draw(0 + this.shakeOffsetX, 0 + this.shakeOffsetY);
         this.LOGO_ARMOR.draw((SCREEN_WIDTH - this.LOGO_ARMOR.width) / 2 + this.shakeOffsetX, 0 + this.shakeOffsetY);
 
-        if (!this.introAnimationDone) {
-            const blueDone = animateWithEasing(this.BG_BLUE_GUY, { x: 0 }, Easing.linear, 600);
-            const whiteDone = animateWithEasing(this.BG_WHITE_GUY, { x: SCREEN_WIDTH - this.BG_WHITE_GUY.width }, Easing.linear, 600);
+        const shouldDrawGuys = !this.isMultiplayerMode || this.isTransitioning;
 
-            if (blueDone && whiteDone) {
-                this.introAnimationDone = true;
-                this.flashRequested = true;
+        if (shouldDrawGuys) {
+            if (!this.isMultiplayerMode && !this.isTransitioning && !this.introAnimationDone) {
+                if (!this._introAnimStarted) {
+                    delete this.BG_BLUE_GUY._deltas;
+                    delete this.BG_BLUE_GUY._base;
+                    delete this.BG_BLUE_GUY.start;
+                    delete this.BG_WHITE_GUY._deltas;
+                    delete this.BG_WHITE_GUY._base;
+                    delete this.BG_WHITE_GUY.start;
+                    this._introAnimStarted = true;
+                }
+
+                const blueDone = animateWithEasing(this.BG_BLUE_GUY, { x: 0 }, Easing.linear, 600);
+                const whiteDone = animateWithEasing(this.BG_WHITE_GUY, { x: SCREEN_WIDTH - this.BG_WHITE_GUY.width }, Easing.linear, 600);
+
+                if (blueDone && whiteDone) {
+                    this.introAnimationDone = true;
+                    this._introAnimStarted = false;
+                    this.flashRequested = true;
+                }
+            }
+            this.BG_BLUE_GUY.draw(this.BG_BLUE_GUY.x + this.shakeOffsetX, SCREEN_HEIGHT - this.BG_BLUE_GUY.height + this.shakeOffsetY);
+            this.BG_WHITE_GUY.draw(this.BG_WHITE_GUY.x + this.shakeOffsetX, SCREEN_HEIGHT - this.BG_WHITE_GUY.height + this.shakeOffsetY);
+        }
+    }
+
+    drawMultiplayerButtons() {
+        for (let index = 0; index < this.multiplayerButtons.length; index++) {
+            if (this.selectedIndex === index) {
+                setAnimation(this.multiplayerButtons[index], "hover");
+            } else {
+                setAnimation(this.multiplayerButtons[index], "normal");
             }
         }
 
-        this.BG_BLUE_GUY.draw(this.BG_BLUE_GUY.x + this.shakeOffsetX, SCREEN_HEIGHT - this.BG_BLUE_GUY.height + this.shakeOffsetY);
-        this.BG_WHITE_GUY.draw(this.BG_WHITE_GUY.x + this.shakeOffsetX, SCREEN_HEIGHT - this.BG_WHITE_GUY.height + this.shakeOffsetY);
+        this.CHOOSE_PLAYERS_LABEL.draw(this.CHOOSE_PLAYERS_LABEL.x + this.shakeOffsetX, this.CHOOSE_PLAYERS_LABEL.y + this.shakeOffsetY);
+
+        animationSprite(this.BTN_2_PLAYERS);
+        this.BTN_2_PLAYERS.draw(this.BTN_2_PLAYERS.x + this.shakeOffsetX, this.BTN_2_PLAYERS.y + this.shakeOffsetY);
+
+        animationSprite(this.BTN_3_PLAYERS);
+        this.BTN_3_PLAYERS.draw(this.BTN_3_PLAYERS.x + this.shakeOffsetX, this.BTN_3_PLAYERS.y + this.shakeOffsetY);
+
+        animationSprite(this.BTN_4_PLAYERS);
+        this.BTN_4_PLAYERS.draw(this.BTN_4_PLAYERS.x + this.shakeOffsetX, this.BTN_4_PLAYERS.y + this.shakeOffsetY);
+
+        animationSprite(this.BTN_BACK);
+        this.BTN_BACK.draw(this.BTN_BACK.x + this.shakeOffsetX, this.BTN_BACK.y + this.shakeOffsetY);
     }
 
-    drawButtons() {
+    drawMainMenuButtons() {
         for (let index = 0; index < this.buttons.length; index++) {
             if (this.selectedIndex === index) {
                 setAnimation(this.buttons[index], "hover");
@@ -226,33 +335,90 @@ export default class MainMenuScreen extends ScreenBase {
         this.BTN_SCORES.draw(this.BTN_SCORES.x + this.shakeOffsetX, this.BTN_SCORES.y + this.shakeOffsetY);
     }
 
+    drawButtons() {
+        if (this.isTransitioning && this.targetMode) {
+            return this.targetMode === 'multiplayer' ? this.drawMultiplayerButtons() : this.drawMainMenuButtons();
+        }
+        return this.isMultiplayerMode ? this.drawMultiplayerButtons() : this.drawMainMenuButtons();
+    }
+
     handleInput() {
         const stick = Gamepad.player(PLAYER_ONE_PORT).leftStick();
+        const justPressedUp = Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.UP) || (stick.y < -0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_UP));
+        const justPressedDown = Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.DOWN) || (stick.y > 0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_DOWN));
+        const justPressedLeft = Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.LEFT) || (stick.x < -0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_LEFT));
+        const justPressedRight = Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.RIGHT) || (stick.x > 0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_RIGHT));
 
-        if (Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.UP) ||
-            (stick.y < -0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_UP))) {
-            if (this.selectedIndex === 1) this.selectedIndex = 0;
-            else if (this.selectedIndex === 2 || this.selectedIndex === 3) this.selectedIndex = 1;
-        }
+        if (this.isMultiplayerMode) {
+            if (justPressedLeft) {
+                this.selectedIndex = Math.max(0, this.selectedIndex - 1);
+            }
+            if (justPressedRight) {
+                this.selectedIndex = Math.min(this.multiplayerButtons.length - 2, this.selectedIndex + 1);
+            }
+            if (justPressedUp) {
+                if (this.selectedIndex === 3) this.selectedIndex = 0;
+                else if (this.selectedIndex < 3) this.selectedIndex = 3;
+            }
+            if (justPressedDown) {
+                if (this.selectedIndex < 3) this.selectedIndex = 3;
+                else this.selectedIndex = 0;
+            }
+        } else {
+            if (justPressedUp) {
+                if (this.selectedIndex === 0) this.selectedIndex = this.buttons.length - 1;
+                else if (this.selectedIndex === 1) this.selectedIndex = 0;
+                else if (this.selectedIndex === 2 || this.selectedIndex === 3) this.selectedIndex = 1;
+            }
 
-        if (Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.DOWN) ||
-            (stick.y > 0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_DOWN))) {
-            if (this.selectedIndex === 0) this.selectedIndex = 1;
-            else if (this.selectedIndex === 1) this.selectedIndex = 2;
-        }
+            if (justPressedDown) {
+                if (this.selectedIndex === this.buttons.length - 1) this.selectedIndex = 0;
+                else if (this.selectedIndex === 0) this.selectedIndex = 1;
+                else if (this.selectedIndex === 1) this.selectedIndex = 2;
+            }
 
-        if (Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.LEFT) ||
-            (stick.x < -0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_LEFT))) {
-            if (this.selectedIndex === 3) this.selectedIndex = 2;
-        }
+            if (justPressedLeft) {
+                if (this.selectedIndex === 3) this.selectedIndex = 2;
+            }
 
-        if (Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.RIGHT) ||
-            (stick.x > 0.5 && Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.L_STICK_RIGHT))) {
-            if (this.selectedIndex === 2) this.selectedIndex = 3;
+            if (justPressedRight) {
+                if (this.selectedIndex === 2) this.selectedIndex = 3;
+            }
         }
 
         if (Gamepad.player(PLAYER_ONE_PORT).justPressed(Pads.CROSS)) {
             this.handleSelection();
+        }
+    }
+
+    handleTransitions() {
+        if (!this.isTransitioning) return;
+
+        if (!this._transitionStarted) {
+            delete this.BG_BLUE_GUY._deltas;
+            delete this.BG_BLUE_GUY._base;
+            delete this.BG_BLUE_GUY.start;
+            delete this.BG_WHITE_GUY._deltas;
+            delete this.BG_WHITE_GUY._base;
+            delete this.BG_WHITE_GUY.start;
+            this._transitionStarted = true;
+            this.selectedIndex = 0;
+        }
+
+        const blueDone = animateWithEasing(this.BG_BLUE_GUY, { x: -this.BG_BLUE_GUY.width }, Easing.linear, 400);
+        const whiteDone = animateWithEasing(this.BG_WHITE_GUY, { x: SCREEN_WIDTH }, Easing.linear, 400);
+
+        if (blueDone && whiteDone) {
+            this.isTransitioning = false;
+            this._transitionStarted = false;
+
+            if (this.targetMode === 'multiplayer') {
+                this.isMultiplayerMode = true;
+            } else if (this.targetMode === 'main_menu') {
+                this.isMultiplayerMode = false;
+                this.introAnimationDone = false;
+            }
+            this.targetMode = null;
         }
     }
 
@@ -261,26 +427,34 @@ export default class MainMenuScreen extends ScreenBase {
             this.SFX_CLICK.play();
         }
 
-        let nextState;
-
-        switch (this.selectedIndex) {
-            case 0:
-                nextState = GAME_STATE.SINGLE_PLAYER;
-                break;
-            case 1:
-                nextState = GAME_STATE.MULTIPLAYER;
-                break;
-            case 2:
-                nextState = GAME_STATE.CREDITS;
-                break;
-            case 3:
-                nextState = GAME_STATE.SCORES;
-                break;
+        if (this.isMultiplayerMode) {
+            if (this.selectedIndex === 3) {
+                this.isTransitioning = true;
+                this.flashRequested = true;
+                this.targetMode = 'main_menu';
+            }
+        } else {
+            let nextState;
+            switch (this.selectedIndex) {
+                case 0:
+                    nextState = GAME_STATE.SINGLE_PLAYER;
+                    StateManager.setState(GAME_STATE.LOADING, { targetState: nextState });
+                    break;
+                case 1:
+                    this.isTransitioning = true;
+                    this.flashRequested = true;
+                    this.targetMode = 'multiplayer';
+                    break;
+                case 2:
+                    nextState = GAME_STATE.CREDITS;
+                    StateManager.setState(GAME_STATE.LOADING, { targetState: nextState });
+                    break;
+                case 3:
+                    nextState = GAME_STATE.SCORES;
+                    StateManager.setState(GAME_STATE.LOADING, { targetState: nextState });
+                    break;
+            }
         }
-
-        StateManager.setState(GAME_STATE.LOADING, {
-            targetState: nextState
-        });
     }
 
     handleEffects() {
@@ -329,8 +503,11 @@ export default class MainMenuScreen extends ScreenBase {
     update(deltaTime) {
         if (!this.isActive) return;
 
-        this.handleInput();
+        this.handleTransitions();
         this.handleEffects();
+        if (!this.isTransitioning) {
+            this.handleInput();
+        }
     }
 
     render() {
@@ -352,6 +529,11 @@ export default class MainMenuScreen extends ScreenBase {
         Assets.free(`${ASSETS_PATH.UI}/btn_multiplayer.png`);
         Assets.free(`${ASSETS_PATH.UI}/btn_scores.png`);
         Assets.free(`${ASSETS_PATH.UI}/btn_credits.png`);
+        Assets.free(`${ASSETS_PATH.UI}/choose_players.png`);
+        Assets.free(`${ASSETS_PATH.UI}/btn_2_players.png`);
+        Assets.free(`${ASSETS_PATH.UI}/btn_3_players.png`);
+        Assets.free(`${ASSETS_PATH.UI}/btn_4_players.png`);
+        Assets.free(`${ASSETS_PATH.UI}/btn_back.png`);
     }
 
     onExit() {
